19d08f777f787444a342d9b6c80bfe7d0fc56a3c
PythonPan <2292551082@qq.com>
2024-10-08 13:32:47 +0800

    reduce disk usage.

--- src/github.rs
+++ src/github.rs
@@ -107,7 +107,7 @@ pub async fn command_fetch_release_distributions(args: &ArgMatches) -> Result<()
         )
         .build()?;
 
-    let release_version_range = pep440_rs::VersionSpecifier::from_str(">=3.9")?;
+    let release_version_range = pep440_rs::VersionSpecifier::from_str(">=3.11")?;
 
     let workflows = client.workflows(org, repo);
 
